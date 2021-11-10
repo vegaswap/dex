fc328ba97c913183ea66673f5a675f296dfa9466
benjyz <ben@vegaswap.io>
2021-11-10 12:27:29 +0700

    hardhat

--- contracts/Broker.sol
+++ contracts/Broker.sol
@@ -21,7 +21,7 @@ contract Broker is Ownable {
     constructor() {
     }
     
-    function depositMargin(uint256 qtyIn) public returns (uint256) {
+    function depositMargin(uint256 qtyIn) public {
         //need approve
         require(IERC20(Registry.BUSD).transferFrom(msg.sender, address(this), qtyIn), "Broker: transfer to broker failed");
     }
@@ -36,15 +36,15 @@ contract Broker is Ownable {
         require(IERC20(token).transfer(msg.sender, amount), "Broker: withdraw margin failed");
     }
 
-    function allowRouter(uint256 qty) public returns (uint256) {        
+    function allowRouter(uint256 qty) public {        
         IERC20(Registry.BUSD).approve(Registry.PANCAKE_ROUTER_ADDRESS, qty);
     }
 
-    function allowRouter(uint256 qtyIn, address token) public returns (uint256) {        
+    function allowRouter(uint256 qtyIn, address token) public {        
         IERC20(token).approve(Registry.PANCAKE_ROUTER_ADDRESS, qtyIn);
     }
 
-    function tradeBuy(uint256 qtyIn, address token) public returns (uint256) {
+    function executeBuy(uint256 qtyIn, address token, address to) public returns (uint256) {
         //address token = Registry.CAKE;
         //TODO calculate slippage
         //BUY
@@ -58,7 +58,7 @@ contract Broker is Ownable {
             qtyIn,
             amountOutmin,
             route,
-            address(this),
+            to,
             deadline
         );
 
@@ -67,10 +67,7 @@ contract Broker is Ownable {
         return amounts[0];
     }
 
-    function tradeSell(uint256 qtyIn, address token) public returns (uint256) {
-        //address token = Registry.CAKE;
-        //TODO calculate slippage
-        //SELL
+    function executeSell(uint256 qtyIn, address token, address to) public returns (uint256) {        
         uint256 amountOutmin = 0; //1 * 10**18;
         address[] memory route = new address[](2);
         route[0] = token;
@@ -81,7 +78,7 @@ contract Broker is Ownable {
             qtyIn,
             amountOutmin,
             route,
-            address(this),
+            to,
             deadline
         );
 
@@ -90,15 +87,45 @@ contract Broker is Ownable {
         return amounts[0];
     }
 
+    function brokerBuy(uint256 qtyIn, address token) public returns (uint256) {
+
+        return executeBuy(qtyIn, token, address(this));
+
+    }
+
+    function brokerSell(uint256 qtyIn, address token) public returns (uint256) {
+
+        return executeBuy(qtyIn, token, address(this));
+
+    }
+
+    //pass through
+    function tradeBuy(uint256 qtyIn, address token) public returns (uint256) {
+    
+        require(IERC20(Registry.BUSD).transferFrom(msg.sender, address(this), qtyIn), "Broker: transfer to broker failed");
+        //tradeBuy(qtyIn, token, address(this))
+        return executeBuy(qtyIn, token, msg.sender);
+
+    }
+
+
+    function tradeSell(uint256 qtyIn, address token) public returns (uint256) {
+
+        require(IERC20(token).transferFrom(msg.sender, address(this), qtyIn), "Broker: transfer to broker failed");
+        //tradeBuy(qtyIn, token, address(this))
+        return executeSell(qtyIn, token, msg.sender);
+        
+    }
+
     function buyCake(uint256 qtyIn) public returns (uint256) {
         address token = Registry.CAKE;
         //TODO calculate slippage
         //BUY
-        uint256 amountOutmin = 0; //1 * 10**18;
+        //uint256 amountOutmin = 0; //1 * 10**18;
         address[] memory route = new address[](2);
         route[0] = Registry.BUSD;
         route[1] = token;
-        uint256 deadline = block.timestamp + 15 minutes;
+        //uint256 deadline = block.timestamp + 15 minutes;
 
         uint256 a = tradeBuy(qtyIn, token);
         return a;
